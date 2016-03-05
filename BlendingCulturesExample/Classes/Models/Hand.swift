b2115d5ffd03ac001c650e2778bc62792ab4af71
sgr-ksmt <melodydance.k.s@gmail.com>
2016-03-06 01:10:48 +0900

    Add `@warn_unused_result`.

--- BlendingCulturesExample/Classes/Models/Hand.swift
+++ BlendingCulturesExample/Classes/Models/Hand.swift
@@ -20,10 +20,12 @@ struct Hand: DataType {
         return cards[index]
     }
     
+    @warn_unused_result
     func addNewItemAtIndex(index: Int) -> Hand {
         return insertItem(Card.randomCard(), atIndex: index)
     }
     
+    @warn_unused_result
     private func insertItem(card: Card, atIndex index: Int) -> Hand {
         var mutableCards = cards
         mutableCards.insert(card, atIndex: index)
@@ -34,12 +36,14 @@ struct Hand: DataType {
         return cards[index]
     }
     
+    @warn_unused_result
     func deleteItemAtIndex(index: Int) -> Hand {
         var mutableCards = cards
         mutableCards.removeAtIndex(index)
         return Hand(cards: mutableCards)
     }
     
+    @warn_unused_result
     func moveItem(fromIndex: Int, toIndex: Int) -> Hand {
         return deleteItemAtIndex(fromIndex)
                .insertItem(cards[fromIndex], atIndex: toIndex)
